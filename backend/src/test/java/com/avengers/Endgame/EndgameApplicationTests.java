28806234877a3a4de5b3b8214d30e3e83c389ac6
Aidan Jones <aidanjonesdev@gmail.com>
2026-09-21 21:11:35 +0000

    Add: Skeleton for MT and FE

---
+++ backend/src/test/java/com/avengers/Endgame/EndgameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.avengers.Endgame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EndgameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
