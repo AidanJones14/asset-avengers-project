6252890819e1f48382e5d0dd89ff4d9c67c27b86
anikatahmed <anika.ahmedextra14@gmail.com>
2026-09-24 19:01:51 +0000

    Added user entity

---
+++ backend/src/main/java/com/avengers/endgame/user/User.java
@@ -0,0 +1,11 @@
+package com.avengers.endgame.user;
+
+import java.util.UUID;
+
+public class User {
+    private UUID userId;
+    private String Name;
+    private UserRole Role;
+
+    public User() {}
+}
---
+++ backend/src/main/java/com/avengers/endgame/user/UserRole.java
@@ -0,0 +1,7 @@
+package com.avengers.endgame.user;
+
+public enum UserRole {
+    ADMIN,
+    ANALYST,
+    CLIENT
+}
